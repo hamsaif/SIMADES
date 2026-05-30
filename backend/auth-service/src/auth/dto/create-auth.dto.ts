02e99b853692c85378e0ed9a9dffd55ab7656e74
hamsaif <ilhamilhamilham05@gmail.com>
2026-05-31 00:14:29 +0700

    set create dto auth

--- backend/auth-service/src/auth/dto/create-auth.dto.ts
+++ backend/auth-service/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,15 @@
-export class CreateAuthDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateAuthDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
