b0bf2e885e158cccb38c5a74351121b14fecfc05
hamsaif <ilhamilhamilham05@gmail.com>
2026-05-31 00:19:35 +0700

    login dto auth

---
+++ backend/auth-service/src/auth/dto/login-auth.dto.ts
@@ -0,0 +1,14 @@
+import {
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class LoginAuthDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
