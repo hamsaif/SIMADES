530a09de03ed2204492bbefc80d2f9449528d6db
hamsaif <ilhamilhamilham05@gmail.com>
2026-05-31 00:30:48 +0700

    set auth module

--- backend/auth-service/src/auth/auth.module.ts
+++ backend/auth-service/src/auth/auth.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+import { JwtAccessStrategy } from './strategies/jwt.access.strategy';
+
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
+  imports: [
+    PassportModule,
+
+    JwtModule.register({
+      secret: 'SIMADES_SECRET',
+
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService],
+
+  providers: [AuthService, PrismaService, JwtAccessStrategy],
 })
 export class AuthModule {}
