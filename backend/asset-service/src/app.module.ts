7cad7386a1541866c5cfb52351d157ec52423670
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-07 00:58:34 +0700

    membuat folder asset'

--- backend/asset-service/src/app.module.ts
+++ backend/asset-service/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KategoriModule } from './kategori/kategori.module';
 import { PrismaService } from './prisma.service';
+import { AssetModule } from './asset/asset.module';
 
 @Module({
-  imports: [KategoriModule],
+  imports: [KategoriModule, AssetModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
