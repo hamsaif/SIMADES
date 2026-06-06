bdde3a343bad07dc5a98600aa25c3dce863fff29
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-06 22:45:44 +0700

    import prisma ke app module asset-service

--- backend/asset-service/src/app.module.ts
+++ backend/asset-service/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KategoriModule } from './kategori/kategori.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [KategoriModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
