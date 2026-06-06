1da4d79a3ecf5a1726c4fecbb5d58d4a706c3eb3
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-07 01:02:24 +0700

    create asset dto

--- backend/asset-service/src/asset/dto/create-asset.dto.ts
+++ backend/asset-service/src/asset/dto/create-asset.dto.ts
@@ -1,1 +1,43 @@
-export class CreateAssetDto {}
+// eslint-disable-next-line prettier/prettier
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+import { AssetCondition } from '@prisma/client';
+
+export class CreateAssetDto {
+
+  // Nama aset
+  // Contoh: Lampu Jalan RT 01
+  @IsString()
+  @IsNotEmpty()
+  nama: string;
+
+  // Deskripsi aset
+  @IsOptional()
+  @IsString()
+  deskripsi?: string;
+
+  // Lokasi aset
+  // Contoh: Depan Masjid
+  @IsString()
+  @IsNotEmpty()
+  lokasi: string;
+
+  // URL / nama file foto
+  @IsOptional()
+  @IsString()
+  foto?: string;
+
+  // Kondisi aset
+  @IsEnum(AssetCondition)
+  kondisi: AssetCondition;
+
+  // Relasi ke kategori
+  @IsString()
+  @IsNotEmpty()
+  kategoriId: string;
+}
