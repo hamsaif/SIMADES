d3729e708a4c79e6f29ebafe704536edc7a39269
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-06 22:58:42 +0700

    ubah constructor service

--- backend/asset-service/src/kategori/kategori.service.ts
+++ backend/asset-service/src/kategori/kategori.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  adRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { CreateKategoriDto } from './dto/create-kategori.dto';
 import { UpdateKategoriDto } from './dto/update-kategori.dto';
 
 @Injectable()
 export class KategoriService {
+
+  constructor(private prisma: PrismaService) {}
   create(createKategoriDto: CreateKategoriDto) {
     return 'This action adds a new kategori';
   }
