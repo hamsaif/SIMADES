a06ea6294b974b2b5946f3b3b9591b0890c89ddf
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-07 22:32:45 +0700

    create dto report

--- backend/report-service/src/report/dto/create-report.dto.ts
+++ backend/report-service/src/report/dto/create-report.dto.ts
@@ -1,1 +1,39 @@
-export class CreateReportDto {}
+/* eslint-disable prettier/prettier */
+import {
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+} from 'class-validator';
+
+export class CreateReportDto {
+
+    // Nama pelapor
+    @IsOptional()
+    @IsString()
+    namaPelapor?: string;
+
+    // Nomor HP pelapor
+    @IsOptional()
+    @IsString()
+    noHp?: string;
+
+    // Deskripsi kerusakan
+    @IsString()
+    @IsNotEmpty()
+    deskripsi: string;
+
+    // Foto laporan
+    @IsOptional()
+    @IsString()
+    foto?: string;
+
+    // Asset yang dilaporkan
+    @IsString()
+    @IsNotEmpty()
+    assetId: string;
+
+    // Nama asset
+    @IsString()
+    @IsNotEmpty()
+    assetNama: string;
+}
