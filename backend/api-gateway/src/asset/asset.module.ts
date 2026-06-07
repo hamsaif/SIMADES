3edd5e2119353f25e146423183eaf9ea6324c8b4
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 02:29:28 +0700

    controller asset api

--- backend/api-gateway/src/asset/asset.module.ts
+++ backend/api-gateway/src/asset/asset.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AssetController } from './asset.controller';
 
-@Module({})
+@Module({
+  controllers: [AssetController]
+})
 export class AssetModule {}
