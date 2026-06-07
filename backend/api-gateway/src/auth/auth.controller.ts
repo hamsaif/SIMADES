93655183ca6dae76f76fcce7fe590ddc924bb127
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 01:52:01 +0700

    login dan register auth-controller api

--- backend/api-gateway/src/auth/auth.controller.ts
+++ backend/api-gateway/src/auth/auth.controller.ts
@@ -14,6 +14,11 @@ export class AuthController {
     private readonly authService: AuthService,
   ) {}
 
+  @Post('register')
+  register(@Body() body: any) {
+    return this.authService.register(body);
+  }
+
   @Post('login')
   login(@Body() body: any) {
     return this.authService.login(body);
