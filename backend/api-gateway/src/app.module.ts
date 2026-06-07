1c3dbef9edd7c2c8931800c2501143bdb90b476f
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 02:28:51 +0700

    module asset api

--- backend/api-gateway/src/app.module.ts
+++ backend/api-gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { AssetModule } from './asset/asset.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AssetModule],
   controllers: [AppController],
   providers: [AppService],
 })
