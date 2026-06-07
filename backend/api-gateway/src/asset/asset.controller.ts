0f5682c3a2713822f4711cc3da5c1f2b002c8760
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 02:38:53 +0700

    fix controller gateway

--- backend/api-gateway/src/asset/asset.controller.ts
+++ backend/api-gateway/src/asset/asset.controller.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import {
-  Controller,
-  Get,
+    Controller,
+    Get,
 } from '@nestjs/common';
 
 import { AssetService } from './asset.service';
@@ -9,12 +9,35 @@ import { AssetService } from './asset.service';
 @Controller('asset')
 export class AssetController {
 
-  constructor(
-    private readonly assetService: AssetService,
-  ) {}
+    constructor(
+        private readonly assetService: AssetService,
+    ) { }
 
-  @Get()
-  findAll() {
-    return this.assetService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.assetService.findAll();
+    }
+
+    @Post()
+    create(@Body() body: any) {
+        return this.assetService.create(body);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.assetService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() body: any,
+    ) {
+        return this.assetService.update(id, body);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.assetService.remove(id);
+    }
 }
