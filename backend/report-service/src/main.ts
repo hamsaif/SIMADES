4a20bf9767448aa3f3b00ceedfa789b644bce3a4
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-07 22:59:15 +0700

    set main.ts report

--- backend/report-service/src/main.ts
+++ backend/report-service/src/main.ts
@@ -1,8 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Otomatis validasi DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3003);
+
 }
 bootstrap();
