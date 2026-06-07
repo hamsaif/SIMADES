fcf76ba90071f2caf078861e04117f16fea7181d
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-07 22:42:26 +0700

    menambahkan import prisma service di report service

--- backend/report-service/src/report/report.service.ts
+++ backend/report-service/src/report/report.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+} from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma.service';
+
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
 
