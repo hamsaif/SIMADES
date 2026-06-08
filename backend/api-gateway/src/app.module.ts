7843001d5a45c367b96acb8c4714888b53c920b2
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 08:35:39 +0700

    module kategori api

--- backend/api-gateway/src/app.module.ts
+++ backend/api-gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AssetModule } from './asset/asset.module';
+import { KategoriModule } from './kategori/kategori.module';
 
 @Module({
-  imports: [AuthModule, AssetModule],
+  imports: [AuthModule, AssetModule, KategoriModule],
   controllers: [AppController],
   providers: [AppService],
 })
