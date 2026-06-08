82402d91542ec3e532de8c03e4cb796e6f26f0b3
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 08:37:53 +0700

    service kategori api

--- backend/api-gateway/src/kategori/kategori.module.ts
+++ backend/api-gateway/src/kategori/kategori.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { KategoriController } from './kategori.controller';
+import { KategoriService } from './kategori.service';
 
 @Module({
-  controllers: [KategoriController]
+  controllers: [KategoriController],
+  providers: [KategoriService]
 })
 export class KategoriModule {}
