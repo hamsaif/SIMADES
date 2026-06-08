5649d2c8bf066a08971071e68763d782c6f5afd8
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 15:02:32 +0700

    report service gateway api

--- backend/api-gateway/src/report/report.service.ts
+++ backend/api-gateway/src/report/report.service.ts
@@ -1,4 +1,121 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  HttpException,
+  Injectable,
+} from '@nestjs/common';
+
+import axios from 'axios';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+
+  // Ambil seluruh laporan
+  async findAll() {
+
+    try {
+
+      const response =
+        await axios.get(
+          'http://localhost:3003/report',
+        );
+
+      return response.data;
+
+    } catch (error: any) {
+
+      throw new HttpException(
+        error.response.data,
+        error.response.status,
+      );
+    }
+  }
+
+  // Ambil laporan berdasarkan id
+  async findOne(id: string) {
+
+    try {
+
+      const response =
+        await axios.get(
+          `http://localhost:3003/report/${id}`,
+        );
+
+      return response.data;
+
+    } catch (error: any) {
+
+      throw new HttpException(
+        error.response.data,
+        error.response.status,
+      );
+    }
+  }
+
+  // Buat laporan baru
+  async create(body: any) {
+
+    try {
+
+      const response =
+        await axios.post(
+          'http://localhost:3003/report',
+          body,
+        );
+
+      return response.data;
+
+    } catch (error: any) {
+
+      throw new HttpException(
+        error.response.data,
+        error.response.status,
+      );
+    }
+  }
+
+  // Update status laporan
+  async update(
+    id: string,
+    body: any,
+  ) {
+
+    try {
+
+      const response =
+        await axios.patch(
+          `http://localhost:3003/report/${id}`,
+          body,
+        );
+
+      return response.data;
+
+    } catch (error: any) {
+
+      throw new HttpException(
+        error.response.data,
+        error.response.status,
+      );
+    }
+  }
+
+  // Hapus laporan
+  async remove(id: string) {
+
+    try {
+
+      const response =
+        await axios.delete(
+          `http://localhost:3003/report/${id}`,
+        );
+
+      return response.data;
+
+    } catch (error: any) {
+
+      throw new HttpException(
+        error.response.data,
+        error.response.status,
+      );
+    }
+  }
+}
