d9f02372e4a11eb3f7681a14d1841ca7d780ed3d
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 15:03:17 +0700

    controller report gateway api

--- backend/api-gateway/src/report/report.controller.ts
+++ backend/api-gateway/src/report/report.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { ReportService } from './report.service';
 
 @Controller('report')
-export class ReportController {}
+export class ReportController {
+
+  constructor(
+    private readonly reportService: ReportService,
+  ) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.reportService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.reportService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.reportService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: any,
+  ) {
+    return this.reportService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.reportService.remove(id);
+  }
+}
