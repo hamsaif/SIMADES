558eab1a35440082d8b83a1b4b9994c4d197f8c0
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 08:41:30 +0700

    fix controller kategori api

--- backend/api-gateway/src/kategori/kategori.controller.ts
+++ backend/api-gateway/src/kategori/kategori.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { KategoriService } from './kategori.service';
 
 @Controller('kategori')
-export class KategoriController {}
+export class KategoriController {
+
+  constructor(
+    private readonly kategoriService: KategoriService,
+  ) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.kategoriService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.kategoriService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.kategoriService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: any,
+  ) {
+    return this.kategoriService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.kategoriService.remove(id);
+  }
+}
