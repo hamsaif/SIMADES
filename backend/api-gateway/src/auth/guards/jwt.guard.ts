689919fa337761c9f433295ee2e42e53fe574643
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 15:42:46 +0700

    jwt guard danstrategies

---
+++ backend/api-gateway/src/auth/guards/jwt.guard.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtGuard extends AuthGuard('jwt') {}
