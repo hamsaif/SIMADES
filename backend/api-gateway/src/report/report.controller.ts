385685e41b200160e08acb2ba4cca56fc50864ed
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 17:13:11 +0700

    set jwt guard untuk report

--- backend/api-gateway/src/report/report.controller.ts
+++ backend/api-gateway/src/report/report.controller.ts
@@ -1,48 +1,53 @@
 /* eslint-disable prettier/prettier */
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
 } from '@nestjs/common';
 
 import { ReportService } from './report.service';
+import { UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Controller('report')
 export class ReportController {
 
-  constructor(
-    private readonly reportService: ReportService,
-  ) {}
+    constructor(
+        private readonly reportService: ReportService,
+    ) { }
 
-  @Post()
-  create(@Body() body: any) {
-    return this.reportService.create(body);
-  }
+    @Post()
+    create(@Body() body: any) {
+        return this.reportService.create(body);
+    }
+    @UseGuards(JwtGuard)
+    @Get()
+    findAll() {
+        return this.reportService.findAll();
+    }
 
-  @Get()
-  findAll() {
-    return this.reportService.findAll();
-  }
+    @UseGuards(JwtGuard)
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.reportService.findOne(id);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reportService.findOne(id);
-  }
+    @UseGuards(JwtGuard)
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() body: any,
+    ) {
+        return this.reportService.update(id, body);
+    }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() body: any,
-  ) {
-    return this.reportService.update(id, body);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reportService.remove(id);
-  }
+    @UseGuards(JwtGuard)
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.reportService.remove(id);
+    }
 }
