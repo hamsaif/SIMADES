fc42fb1b242d388118310dc2c3ae798d0c27f7e7
hamsaif <ilhamilhamilham05@gmail.com>
2026-06-08 15:06:25 +0700

    import report module

--- backend/api-gateway/src/app.module.ts
+++ backend/api-gateway/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AssetModule } from './asset/asset.module';
 import { KategoriModule } from './kategori/kategori.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [AuthModule, AssetModule, KategoriModule],
+  imports: [AuthModule, AssetModule, KategoriModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
