cf68708894c3c60163308ef2e4e1a7460f434d8e
rofiivor <masterrofi53995@gmail.com>
2026-06-15 18:42:18 +0700

    create report page

--- cms-admin/src/app/(admin)/report/page.tsx
+++ cms-admin/src/app/(admin)/report/page.tsx
@@ -1,3 +1,42 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { getAsset } from '@/services/asset.service';
+
 export default function ReportPage() {
-  return <h1>Report</h1>;
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const response = await getAsset();
+    setTotal(response.data.length);
+  };
+
+  return (
+    <article className="page-container">
+
+      <header className="page-header">
+        <h2 className="page-title">
+          Report Asset
+        </h2>
+      </header>
+
+      <section className="card">
+        <h3>Total Asset</h3>
+
+        <p
+          style={{
+            fontSize: '32px',
+            fontWeight: 'bold',
+          }}
+        >
+          {total}
+        </p>
+      </section>
+
+    </article>
+  );
 }
