8714a635c5d41a9cd81757f224d4df8164139619
rofiivor <masterrofi53995@gmail.com>
2026-06-15 18:02:36 +0700

    add asset state

--- cms-admin/src/app/(admin)/asset/page.tsx
+++ cms-admin/src/app/(admin)/asset/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function AssetPage() {
+  const [asset, setAsset] = useState<any[]>([]);
   return (
     <article>
       <h2>Asset Desa</h2>
