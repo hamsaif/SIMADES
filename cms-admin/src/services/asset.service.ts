5e6cdd156baf09bdd235764e216a1825c9af794f
rofiivor <masterrofi53995@gmail.com>
2026-06-15 17:47:33 +0700

    update asset service

--- cms-admin/src/services/asset.service.ts
+++ cms-admin/src/services/asset.service.ts
@@ -10,3 +10,15 @@ export const createAsset = async (data: any) => {
   return response.data;
 };
 
+export const updateAsset = async (
+  id: string,
+  data: any,
+) => {
+  const response = await api.patch(
+    `/asset/${id}`,
+    data,
+  );
+
+  return response.data;
+};
+
